5d9f92a7136da831ec47c585ced4f3f82d1ddd2d
mernaadell <mernaadel963@gmail.com>
2020-02-15 16:36:10 +0200

    update with numbers ,strings and arrays

--- Section3.js
+++ Section3.js
@@ -1,60 +1,50 @@
-//------functions
-function pow(x,y){
-    console.log(x**y)
-}
-pow(2,5)
-let pw=function pow(x,y){
-    console.log(x**y)
-}
-pow(2,5)
-//higher order fuction
-let pw1=function (){
-    console.log("hi1")
-    return function second(){
-        return 5
-    }
-}
-console.log(pw1())
-//arrow function
-let pw2= first =>{
-    console.log("hi2")
-    
-}
-console.log(pw2(2))
-
-let pw3= () =>{
-    console.log("hi3")
-    
-}
-console.log(pw3())
-let pw4= x =>{ //take one parameter
-    console.log("hi4",x)
-    
-}
-console.log(pw4(1))
-let pw5= x =>x //y3ml return l x
-console.log(pw5(29))
-//pure function
-y=1 
-let inc=(x)=>{ //law adat ll function y hy8yar feha
-    x++
-    return x
-}
-y=inc(y)
-console.log(y)
-//----------scopes
-//ay global a2dar a3mal access mn ay mkan
-//functional scope
-let c=()=>{
-    x=10
-}
-//block scope
-{
-    let d=19 //mesh h2dar awsalo
-    var f=17 //hshofo bra
-    console.log("imin scope")
-}
-//IIFE funcion+call
-(function fun(x){
-    console.log("hello")
-})(10) //mynf3sh a3mlha call tani
+//numbers
+    let x=9864 
+    let y=13.11 //all types are numbers
+    let z=1.311e+1
+    console.log(x.toExponential()) //l numers 3obaa 3n objects gwaha methods
+    console.log(y.toFixed(2)) //round string
+    console.log(y.toString(2)) //binary
+    //infinty &NAN
+    console.log(5/0)//infinty
+    console.log("hi"*2)//nan
+    console.log(isNaN(NaN))
+//strings
+    console.log("hello\x66") //asci
+    //Template Literals 
+    let s=`hello
+     merna` //by7as b enter
+     let s2='   hi ${}  '
+     console.log(s2)
+     console.log(s2.trim())
+//arrays
+    let arr=[1,2,4,0]
+    console.log(arr)
+    arr[4]=11
+    console.log(arr)
+    console.log(arr.length)
+    console.log(arr.sort())
+    console.log(arr.push(5)) //ydef fe la5er
+    console.log(arr.pop())//y7zaf mn l a5er
+    console.log(arr.shift()) //y7zaf mn awal
+    console.log(arr.unshift(1))//ydef mn awal
+    console.log(arr.splice(3,0,"hi")) //mn awal index 3 aktab hi
+    console.log(arr)
+    console.log(arr.splice(3,2))
+    console.log(arr.fill("*",1,5))
+    console.log(arr.copyWithin(1,0,3))//y7ot eliimfrod kanm7tot mn index 0,3 mn awal 1
+    console.log(arr)
+    console.log(arr.slice(1,2))//ytl3 goz2
+    arr2=[4,3,2]
+    console.log(arr.concat(arr2))
+    console.log(arr.reverse()) //bt8yar fe array
+    console.log(arr.toString()) //yshel brackets
+    console.log(arr.join(""))//yshel ,
+    let arr3="mena".split("")
+    console.log(arr3)
+    console.log(arr2.indexOf(5))
+    console.log(arr2.includes(6))
+    console.log(arr2.find(e=> e>1 ))//awe3onser bs
+    console.log(arr2.filter(e=> e>1 ))
+    console.log(arr2.every(e=> e>1 ))//boolean 3la kol l elements
+    console.log(arr2.some(e=> e>1 ))//wa7ed atleast
