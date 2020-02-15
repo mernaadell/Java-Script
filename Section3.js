804e114da2efbdf403d54a271907ba907547c28f
mernaadell <mernaadel963@gmail.com>
2020-02-16 00:05:31 +0200

    update Destructing Assignment spread opreator etc

--- Section3.js
+++ Section3.js
@@ -48,3 +48,58 @@
     console.log(arr2.filter(e=> e>1 ))
     console.log(arr2.every(e=> e>1 ))//boolean 3la kol l elements
     console.log(arr2.some(e=> e>1 ))//wa7ed atleast
+//Mapping
+    arr.forEach(e=>console.log(e)) //loop bta5od call back function
+   let a=arr.map(e=>e++)//l nateg elii b3mal 3leh op arg3ofe arr gdeda call back function a3ml elii ana 3wzah fe array elements
+   console.log(a)
+   console.log(arr.reduce((p,c)=>p+c)) //bta5od 2 parameters previous =0and current awal element
+   console.log(arr.reduceRight((p,c)=>p+c))//mn l ymen
+   console.log(typeof arr)
+   console.log(Array.isArray(arr))
+//2D array
+    let d2=[
+        [1,2],
+        [2,1]
+    ]
+    console.log(d2[1][1])
+    for(let i of d2){
+        for(let j of i)
+        {
+            console.log(j)
+        }
+
+    }
+    console.log("len",d2.length)
+    for(let i=0 ;i<d2.length;i++){
+        for(let j =0;j<d2.length;j++)
+        {
+            console.log(d2[i][j])
+        }
+    }
+//Destructing Assignment 
+    let [a1,b1,c1,d1]=arr
+    let ob={
+        a:1,
+        b:2,
+        c:3
+    }
+    let {f,g,h}=ob
+    console.log(f)
+//Spread Operator
+    let g2=[1,2,3]
+    function sum(a,b,c){
+        let sum=0
+        g2.map(e=>sum=+e)
+        return sum
+
+    }
+    console.log(sum(...g2)) //lazem ...
+    let [c,...z2]=g2
+    console.log(z2) //array of 2 elements
+//Array Constructor
+    let u=Array(5)
+    console.log(u)
+    let u2=Array.of(1,2,3)
+    console.log(u2)
+
+
